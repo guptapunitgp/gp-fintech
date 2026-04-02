e56c12eec2aefd03117695943ea6266361b6fb42
Punit Gupta <guptapunitgp@gmail.com>
2026-04-02 15:35:03 +0530

    fix issue

--- server/src/lib/api.js
+++ server/src/lib/api.js
@@ -1,14 +1,7 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  (import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : "https://gp-fintech.onrender.com");
-
 const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
+  baseURL: "https://gp-fintech.onrender.com/api",
 });
 
 export default api;
